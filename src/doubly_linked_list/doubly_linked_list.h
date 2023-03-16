82a99cf3ad864ab9cacbc8110ea75f3a44b78883
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-16 09:06:29 -0300

    Create function to get length property.

--- src/doubly_linked_list/doubly_linked_list.h
+++ src/doubly_linked_list/doubly_linked_list.h
@@ -14,6 +14,10 @@ typedef t_doubly_linked_list t_dl_list;
 
 t_dl_list	*new_node_dl_list(int value);
 
+// Properties
+
+int			length_dl_list(t_dl_list *head);
+
 // Handlers
 
 int			connect_nodes_dl_list(t_dl_list *left, t_dl_list *right);
@@ -21,7 +25,7 @@ int			transform_into_circular_dl_list(t_dl_list *head);
 
 // Destructors
 
-void	delete_list_dl_list(t_dl_list **head);
-void	delete_node_dl_list(t_dl_list *node);
+void		delete_list_dl_list(t_dl_list **head);
+void		delete_node_dl_list(t_dl_list *node);
 
 #endif
