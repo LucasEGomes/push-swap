14cf228f10390ede1d64bb9c4b1e08c7d4fba604
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-16 23:14:24 -0300

    Create swap operations.

---
+++ src/operations/swap.c
@@ -0,0 +1,37 @@
+#include "application/application.h"
+#include <unistd.h>
+
+static void	swap(t_dl_list **head)
+{
+	t_dl_list	*temp;
+
+	temp = *head;
+	*head = temp->next;
+	connect_nodes_dl_list(temp->prev, temp->next);
+	connect_nodes_dl_list(temp, (*head)->next);
+	connect_nodes_dl_list(*head, temp);
+}
+
+void	swap_a(t_app *app, int verbose)
+{
+	if (app->size_a > 1)
+		swap(&(app->stack_a));
+	if (verbose)
+		write(STDOUT_FILENO, "sa", 2);
+}
+
+void	swap_b(t_app *app, int verbose)
+{
+	if (app->size_b > 1)
+		swap(&(app->stack_b));
+	if (verbose)
+		write(STDOUT_FILENO, "sb", 2);
+}
+
+void	swap_both(t_app *app, int verbose)
+{
+	swap_a(app, 0);
+	swap_b(app, 0);
+	if (verbose)
+		write(STDOUT_FILENO, "ss", 2);
+}
