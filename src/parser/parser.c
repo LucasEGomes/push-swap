f34049077703c25f683900f05a7e47c9a19b3dbf
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-18 03:33:11 -0300

    Transform comparisons into functions for better readability. Transform positive and negative number loops into functions for better readability.

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,6 +8,47 @@ static t_dl_list	*abort_parse(t_dl_list **node)
 	return (NULL);
 }
 
+static int is_int_underflow(int current, int addend)
+{
+	const int	int_min = -__INT_MAX__ - 1;
+
+	return (current < int_min / 10 || current * 10 < int_min + addend);
+}
+
+static int is_int_overflow(int current, int addend)
+{
+	return (current > __INT_MAX__ / 10 || current * 10 > __INT_MAX__ - addend);
+}
+
+static t_dl_list	*negative_number(t_dl_list *node, char *number)
+{
+	number++;
+	while (is_digit(*number))
+	{
+		if (is_int_underflow(node->value, *number - '0'))
+			return (abort_parse(&node));
+		node->value = node->value * 10 - (*number - '0');
+		number++;
+	}
+	if (!is_space_or_null_byte(*number))
+		return (abort_parse(&node));
+	return (node);
+}
+
+static t_dl_list	*positive_number(t_dl_list *node, char *number)
+{
+	while (is_digit(*number))
+	{
+		if (is_int_overflow(node->value, *number - '0'))
+			return (abort_parse(&node));
+		node->value = node->value * 10 + (*number - '0');
+		number++;
+	}
+	if (!is_space_or_null_byte(*number))
+		return (abort_parse(&node));
+	return (node);
+}
+
 t_dl_list	*parse_number(char *number)
 {
 	t_dl_list	*node;
@@ -17,29 +58,8 @@ t_dl_list	*parse_number(char *number)
 		return (NULL);
 	node->value = 0;
 	if (*number == '-')
-	{
-		number++;
-		while (is_digit(*number))
-		{
-			if (node->value < (-__INT_MAX__ - 1) / 10 || node->value * 10 < (-__INT_MAX__ - 1) + (*number - '0'))
-				return (abort_parse(&node));
-			node->value = node->value * 10 - (*number - '0');
-			number++;
-		}
-		if (!is_space_or_null_byte(*number))
-			return (abort_parse(&node));
-		return (node);
-	}
-	while (is_digit(*number))
-	{
-		if (node->value > __INT_MAX__ / 10 || node->value * 10 > __INT_MAX__ - (*number - '0'))
-			return (abort_parse(&node));
-		node->value = node->value * 10 + (*number - '0');
-		number++;
-	}
-	if (!is_space_or_null_byte(*number))
-		return (abort_parse(&node));
-	return (node);
+		return (negative_number(node, number));
+	return (positive_number(node, number));
 }
 
 static t_dl_list	*create_head(char *string)
