05453bb73e7105661c0bbd18f0ffb503f0aa2161
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-19 20:50:27 -0300

    Add function to stop program and free resources.

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,7 +1,8 @@
 #include "application/application.h"
 #include "entities/doubly_linked_list/doubly_linked_list.h"
 #include "parser/parser.h"
-#include <stddef.h>
+#include <unistd.h>
+
 
 static int	create_resources(t_dl_list **stack_a, int argc, char **argv)
 {
@@ -24,6 +25,13 @@ static void	free_resources(t_dl_list *stack_a)
 	delete_list_dl_list(&stack_a);
 }
 
+static int	throw_program_exception(t_dl_list *stack_a)
+{
+	free_resources(stack_a);
+	write(STDERR_FILENO, "Error\n", 6);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
 	t_dl_list	*stack_a;
@@ -33,12 +41,9 @@ int	main(int argc, char **argv)
 		return (0);
 	stack_a = NULL;
 	if (create_resources(&stack_a, argc, argv) != 0)
-		return (1);
-	if (start_application(stack_a) != 0)
-	{
-		free_resources(stack_a);
-		return (1);
-	}
+		return (throw_program_exception(NULL));
+	if (run_application(stack_a) != 0)
+		return (throw_program_exception(stack_a));
 	free_resources(stack_a);
 	return (0);
 }
