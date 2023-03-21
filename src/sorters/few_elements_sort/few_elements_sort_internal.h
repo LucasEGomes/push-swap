e599d973e1e01507b1700151820d80883215b2b7
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 15:19:37 -0300

    Refactor function to sort 3 elements to use a generic one for reusability.

--- src/sorters/few_elements_sort/few_elements_sort_internal.h
+++ src/sorters/few_elements_sort/few_elements_sort_internal.h
@@ -4,6 +4,20 @@
 
 typedef int (*t_sorter)(t_push_swap *push_swap, int verbose);
 
+// Patterns of 3 elements
+
+int	sort_0_2_1(t_push_swap *push_swap, int is_stack_a, int verbose);
+int	sort_1_0_2(t_push_swap *push_swap, int is_stack_a, int verbose);
+int	sort_1_2_0(t_push_swap *push_swap, int is_stack_a, int verbose);
+int	sort_2_0_1(t_push_swap *push_swap, int is_stack_a, int verbose);
+int	sort_2_1_0(t_push_swap *push_swap, int is_stack_a, int verbose);
+
+// Generic sorter
+
+int	sort_stack_3_elements(t_push_swap *push_swap, int is_stack_a, int verbose);
+
+// Sorters
+
 int	sort_2_elements(t_push_swap *push_swap, int verbose);
 int	sort_3_elements(t_push_swap *push_swap, int verbose);
 int	sort_4_elements(t_push_swap *push_swap, int verbose);
--- src/sorters/few_elements_sort/sort_3_elements.c
+++ src/sorters/few_elements_sort/sort_3_elements.c
@@ -1,46 +1,25 @@
+#include "few_elements_sort_internal.h"
 #include "entities/push_swap/push_swap.h"
 #include "operations/operations.h"
 
-static int	sort_0_2_1(t_push_swap *push_swap, int verbose)
-{
-	swap_a(push_swap, verbose);
-	rotate_a(push_swap, verbose);
-	return (2);
-}
-
-static int	sort_1_0_2(t_push_swap *push_swap, int verbose)
-{
-	swap_a(push_swap, verbose);
-	return (1);
-}
-
-static int	sort_1_2_0(t_push_swap *push_swap, int verbose)
-{
-	reverse_rotate_a(push_swap, verbose);
-	return (1);
-}
-
-static int	sort_2_0_1(t_push_swap *push_swap, int verbose)
-{
-	rotate_a(push_swap, verbose);
-	return (1);
-}
-
-int	sort_3_elements(t_push_swap *push_swap, int verbose)
+int	sort_stack_3_elements(t_push_swap *push_swap, int is_stack_a, int verbose)
 {
 	if (push_swap->stack_a->value < push_swap->stack_a->next->value)
 	{
 		if (push_swap->stack_a->next->value < push_swap->stack_a->prev->value)
 			return (0);
 		if (push_swap->stack_a->value > push_swap->stack_a->prev->value)
-			return (sort_1_2_0(push_swap, verbose));
-		return (sort_0_2_1(push_swap, verbose));
+			return (sort_1_2_0(push_swap, is_stack_a, verbose));
+		return (sort_0_2_1(push_swap, is_stack_a, verbose));
 	}
 	if (push_swap->stack_a->value < push_swap->stack_a->prev->value)
-		return (sort_1_0_2(push_swap, verbose));
+		return (sort_1_0_2(push_swap, is_stack_a, verbose));
 	if (push_swap->stack_a->next->value < push_swap->stack_a->prev->value)
-		return (sort_2_0_1(push_swap, verbose));
-	swap_a(push_swap, verbose);
-	reverse_rotate_a(push_swap, verbose);
-	return (2);
+		return (sort_2_0_1(push_swap, is_stack_a, verbose));
+	return (sort_2_1_0(push_swap, is_stack_a, verbose));
+}
+
+int	sort_3_elements(t_push_swap *push_swap, int verbose)
+{
+	return (sort_stack_3_elements(push_swap, 1, verbose));
 }
