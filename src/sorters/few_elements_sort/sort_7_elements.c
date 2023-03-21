718469c87373fb14d76a0296d9c2d12812ee4f5d
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 18:06:03 -0300

    Fix 7 elements infinite loop.

--- src/sorters/few_elements_sort/sort_7_elements.c
+++ src/sorters/few_elements_sort/sort_7_elements.c
@@ -1,12 +1,14 @@
 #include "few_elements_sort_internal.h"
 #include "entities/push_swap/push_swap.h"
+#include "operations/operations.h"
 #include "helper/helper.h"
 
 int	sort_7_elements(t_push_swap *push_swap, int verbose)
 {
 	int	steps;
 
-	steps = move_stack_a_to_value(push_swap, 1, verbose);
+	steps = move_stack_a_to_value(push_swap, 0, verbose);
+	steps += rotate_a(push_swap, verbose);
 	steps += split_stack_a(push_swap, 3, verbose);
 	steps += sort_4_elements(push_swap, verbose);
 	steps += sort_stack_3_elements(push_swap, 0, verbose);
