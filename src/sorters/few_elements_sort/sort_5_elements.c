df522d892b671b9c1f38065314795c42f382f143
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 10:39:32 -0300

    Create function to sort 5 elements.

---
+++ src/sorters/few_elements_sort/sort_5_elements.c
@@ -0,0 +1,18 @@
+#include "few_elements_sort_internal.h"
+#include "entities/push_swap/push_swap.h"
+#include "operations/operations.h"
+#include "helper/helper.h"
+
+int	sort_5_elements(t_push_swap *push_swap, int verbose)
+{
+	int	steps;
+
+	push_b(push_swap, verbose);
+	push_b(push_swap, verbose);
+	steps = 2;
+	steps += sort_3_elements(push_swap, verbose);
+	steps += insert_top_b_into_a(push_swap, verbose);
+	steps += insert_top_b_into_a(push_swap, verbose);
+	steps += move_stack_a_to_zero(push_swap, verbose);
+	return (steps);
+}
