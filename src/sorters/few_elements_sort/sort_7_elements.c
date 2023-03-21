9e56e33ed2c08a710bf266a06b5f3d44381e99c4
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 15:23:33 -0300

    Create function to sort 7 elements.

---
+++ src/sorters/few_elements_sort/sort_7_elements.c
@@ -0,0 +1,16 @@
+#include "few_elements_sort_internal.h"
+#include "entities/push_swap/push_swap.h"
+#include "helper/helper.h"
+
+int	sort_7_elements(t_push_swap *push_swap, int verbose)
+{
+	int	steps;
+
+	steps = move_stack_a_to_value(push_swap, 1, verbose);
+	steps += split_stack_a(push_swap, 3, verbose);
+	steps += sort_stack_3_elements(push_swap, 1, verbose);
+	steps += sort_stack_3_elements(push_swap, 0, verbose);
+	steps += insert_range_b_into_a(push_swap, 3, verbose);
+	steps += move_stack_a_to_value(push_swap, 1, verbose);
+	return (steps);
+}
