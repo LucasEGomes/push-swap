b4542ef7154b64b0fb780f1b2dd8448d1ab2b982
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 15:59:00 -0300

    Apply new sorter into application.

--- src/application/run.c
+++ src/application/run.c
@@ -2,6 +2,7 @@
 #include "entities/push_swap/push_swap.h"
 #include "operations/operations.h"
 #include "sorters/bubble_sort/bubble_sort.h"
+#include "sorters/few_elements_sort/few_elements_sort.h"
 #include "sorters/merge_sort/merge_sort.h"
 #include "helper/helper.h"
 #include <stddef.h>
@@ -51,16 +52,21 @@ static int	free_application_resources(t_push_swap *push_swap, int return_code)
 int	run_application(t_dl_list *stack_a)
 {
 	t_push_swap	push_swap;
-	t_push_swap	sorted;
+	t_push_swap	auxiliary;
 
 	initialize_push_swap(&push_swap, stack_a);
-	if (copy_push_swap(&push_swap, &sorted))
+	if (copy_push_swap(&push_swap, &auxiliary))
 		return (1);
-	bubble_sort(&sorted, 0);
-	if (has_duplications(sorted.stack_a))
-		return (free_application_resources(&sorted, 1));
-	replace_with_index_push_swap(&sorted, &push_swap);
-	setup(&push_swap, 0, 50, 1);
-	natural_merge_sort(&push_swap, 1);
-	return (free_application_resources(&sorted, 0));
+	bubble_sort(&auxiliary, 0);
+	if (has_duplications(auxiliary.stack_a))
+		return (free_application_resources(&auxiliary, 1));
+	replace_with_index_push_swap(&auxiliary, &push_swap);
+	if (push_swap.size_a <= 8)
+		few_elements_sort(&push_swap, 1);
+	else
+	{
+		setup(&push_swap, 0, 50, 1);
+		natural_merge_sort(&push_swap, 1);
+	}
+	return (free_application_resources(&auxiliary, 0));
 }
