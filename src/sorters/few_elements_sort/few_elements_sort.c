02af802be15f52ae545614c14947c62b69fbe44c
Lucas E. Gomes <lucasedugomes@gmail.com>
2023-03-21 15:53:25 -0300

    Add error treatment to few elements sorter. Remove some sorters from array.

--- src/sorters/few_elements_sort/few_elements_sort.c
+++ src/sorters/few_elements_sort/few_elements_sort.c
@@ -3,17 +3,17 @@
 
 int	few_elements_sort(t_push_swap *push_swap, int verbose)
 {
-	const t_sorter	sorters[9] = {
+	const t_sorter	sorters[7] = {
 		sort_2_elements,
 		sort_3_elements,
 		sort_4_elements,
 		sort_5_elements,
 		sort_6_elements,
 		sort_7_elements,
-		sort_8_elements,
-		sort_9_elements,
-		sort_10_elements
+		sort_8_elements
 	};
 
+	if (push_swap->size_a > 8)
+		return (0);
 	return (sorters[push_swap->size_a - 2](push_swap, verbose));
 }
